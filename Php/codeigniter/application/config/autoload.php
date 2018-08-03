98185fe1d66682d1d8c3e97ad344c3452c2eb475
Lucimar <Aluno@BG-LAB5-2>
2018-08-03 19:52:23 -0300

    Commit 5

--- Php/codeigniter/application/config/autoload.php
+++ Php/codeigniter/application/config/autoload.php
@@ -3,9 +3,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $autoload['packages'] = array();
-$autoload['libraries'] = array('database', 'session');
+$autoload['libraries'] = array('database', 'session', 'form_validation');
 $autoload['drivers'] = array();
 $autoload['helper'] = array('url', 'file');
 $autoload['config'] = array();
 $autoload['language'] = array();
-$autoload['model'] = array();
+$autoload['model'] = array('contatos_model');
