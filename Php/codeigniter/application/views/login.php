da21e7c48afe6ede3ea75e4d5a02263749dbb133
Lucimar <Aluno@BG-LAB5-2>
2018-08-03 20:24:10 -0300

    Commit 8

--- Php/codeigniter/application/views/login.php
+++ Php/codeigniter/application/views/login.php
@@ -1,7 +1,16 @@
 <title>Login</title>
 
-</header>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap-3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap-4/css/formLogin.css" rel="stylesheet" type="text/css"/>
+    <link href="fontawesome-5/web-fonts-with-css/css/fontawesome.min.css" rel="stylesheet"/>
+    <script src="bootstrap-3/js/jquery.js"></script> 
+    <script src="bootstrap-3/js/bootstrap.min.js"></script>        
+    <script src="fontawesome-5/svg-with-js/js/fontawesome.min.js"/></script>
+</head>
+<body>
         <form method="post" class="formLogin" action="<?php echo base_url() . 'Login/efetua_login' ?>">
             <div class="container col-md-offset-3 col-md-4 col-lg-offset-3 col-lg-4 col-sm-12 col-xs-12">
                 <div class="card">
